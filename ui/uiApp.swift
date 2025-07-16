d0e64be453c4d31726102fea7fe9576c7ee69d3a
Amir Abaris <thisisamirabaris@gmail.com>
2025-07-16 21:55:52 +0330

    Initial Commit

---
+++ ui/uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  uiApp.swift
+//  ui
+//
+//  Created by Amir Abaris on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
